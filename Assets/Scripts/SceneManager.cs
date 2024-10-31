f70a707581bbc13e3d6ef389186c8c8539e354a1
Abdul Mukheem Shaik <mukheemuddin@gmail.com>
2024-10-31 13:53:33 +0100

    Added volume to the scene and also changed the camera position on top of the car

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Varjo.XR;
+using Varjo.XR.Input;
 
 public class SceneManager : MonoBehaviour
 {
+
+    void Awake()
+    {
+        //VarjoCamera.SetTrackingMode(VarjoCamera.TrackingMode.RotationOnly);
+        //VarjoHMD
+    }
     // Start is called before the first frame update
     void Start()
     {
